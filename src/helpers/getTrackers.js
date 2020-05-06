c63eb5910064dbd391fbd92038299e08bf2a7b6c
mirpo <miroslavpokrovskiy@gmail.com>
2020-05-06 16:26:31 +0300

    Add getTrackers test

--- src/helpers/getTrackers.js
+++ src/helpers/getTrackers.js
@@ -3,8 +3,14 @@ const { Contract, providers: { JsonRpcProvider } } = require('ethers')
 const getTrackers = async (address, config, jsonRpcProvider) => {
     // eslint-disable-next-line import/no-dynamic-require,global-require
     const trackerRegistryConfig = require(`../../configs/${config}`)
+
     const provider = new JsonRpcProvider(jsonRpcProvider)
+    // check that provider is connected and has some valid blockNumber
+    await provider.getBlockNumber()
+
     const contract = new Contract(address, trackerRegistryConfig.abi, provider)
+    // check that contract is connected
+    await contract.addressPromise
 
     const trackers = []
     const result = await contract.getNodes()
