f364d057aaf21cec4f576521d08b78ab162067ea
mirpo <miroslavpokrovskiy@gmail.com>
2020-05-18 09:53:19 +0300

    Update smoke_test

--- .travis_scripts/smoke_test.sh
+++ .travis_scripts/smoke_test.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+set -e
+
 ## Script for preparing smoke test
 sudo ifconfig docker0 10.200.10.1/24
+
 ## Get Streamr Docker dev
 git clone https://github.com/streamr-dev/streamr-docker-dev.git
+
 ## Switch out image for local one
 sed -i "s#$OWNER/$IMAGE_NAME:dev#$OWNER/$IMAGE_NAME\:taggit#g" $TRAVIS_BUILD_DIR/streamr-docker-dev/docker-compose.override.yml
+
 ## Start up services needed
 $TRAVIS_BUILD_DIR/streamr-docker-dev/streamr-docker-dev/bin.sh start broker-node-no-storage-1 smart-contracts-init --wait
 
@@ -18,8 +24,7 @@ for (( i=0; i < 5; i=i+1 )); do
         sleep $wait_time;
         wait_time=$(( 2*wait_time )) ;
     else
-        break;
+        exit 0
     fi;
 done;
-set -e
-curl http://localhost:8791/api/v1/volume
+exit 1
