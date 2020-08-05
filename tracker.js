66c0971a7ecb770c0d2ad825ffea455dc97257f0
juslesan <santeri.juslenius@gmail.com>
2020-08-05 15:03:52 +0300

    program params

--- tracker.js
+++ tracker.js
@@ -1,6 +1,25 @@
 #!/usr/bin/env node
 const { spawn } = require('child_process')
 const path = require('path')
+const program = require('commander')
+
+const { authenticateFromConfig } = require('./src/helpers/ethereumAuthenticate')
+
+program
+    .option('-g, --generateKey', 'Generate random wallet', false)
+    .option('-p, --privateKey <privateKey>', 'Ethereum private key', null)
+    .option('-n, --trackerName <trackerName>', 'Human readable name', null)
+    .parse(process.argv)
+
+if (!program.generateKey && !program.privateKey) {
+    throw new Error('--generateKey (-g) or --privateKey (-p) parameter required')
+}
+const address = authenticateFromConfig({
+    privateKey: program.privateKey,
+    generateWallet: program.generateKey
+})
+
+const name = program.trackerName || address
 
 spawn(path.resolve(__dirname, './node_modules/streamr-network/bin/tracker.js'), process.argv, {
     cwd: process.cwd(),
