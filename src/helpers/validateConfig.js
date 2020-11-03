60cab06c13bac6c0fb0149bfc1013151f09b0be6
Eric Andrews <eric.andrews@streamr.com>
2020-11-03 09:56:59 +0200

    wip

--- src/helpers/validateConfig.js
+++ src/helpers/validateConfig.js
@@ -5,6 +5,12 @@ const BROKER_CONFIG_SCHEMA = require('./config.schema.json')
 module.exports = function validateConfig(config) {
     const ajv = new Ajv()
     if (!ajv.validate(BROKER_CONFIG_SCHEMA, config)) {
-        throw new Error(ajv.errorsText())
+        throw new Error(ajv.errors.map((e) => {
+            let text = ajv.errorsText([e])
+            if (e.params.additionalProperty) {
+                text += ` (${e.params.additionalProperty})`
+            }
+            return text
+        }).join('\n'))
     }
 }
