be75722c3dc3e1a1551d72b2eb5202fb3efd8866
Teo Gebhard <teo@streamr.com>
2021-02-12 17:27:57 +0200

    Update persistence wait logic

--- test/integration/storage/StorageConfig.test.js
+++ test/integration/storage/StorageConfig.test.js
@@ -1,11 +1,10 @@
 const { startTracker } = require('streamr-network')
 const cassandra = require('cassandra-driver')
 const ethers = require('ethers')
-const fetch = require('node-fetch')
 const { waitForCondition } = require('streamr-test-utils')
 const { StreamMessage } = require('streamr-network').Protocol.MessageLayer
 
-const { startBroker, createClient, addStreamToStorageNode } = require('../../utils')
+const { startBroker, createClient, StorageAssignmentEventManager, waitForStreamPersistedInStorageNode } = require('../../utils')
 
 const contactPoints = ['127.0.0.1']
 const localDataCenter = 'datacenter1'
@@ -13,18 +12,11 @@ const keyspace = 'streamr_dev_v2'
 
 const NODE_HOST = '127.0.0.1'
 const STREAMR_URL = 'http://127.0.0.1'
-const API_URL = `${STREAMR_URL}/api/v1`
 const HTTP_PORT = 17770
 const WS_PORT = 17771
 const TRACKER_PORT = 17772
 const STORAGE_NODE_PORT = 17773
 const BROKER_PORT = 17774
-
-const isPersistent = async (streamId, partition = 0) => {
-    const response = await fetch(`http://${NODE_HOST}:${HTTP_PORT}/api/v1/streams/${encodeURIComponent(streamId)}/storage/partitions/${partition}`)
-    const json = await response.json()
-    return json.persistent
-}
 
 describe('StorageConfig', () => {
     let cassandraClient
@@ -33,6 +25,7 @@ describe('StorageConfig', () => {
     let broker
     let client
     let stream
+    let assignmentEventManager
     const publisherAccount = ethers.Wallet.createRandom()
     const storageNodeAccount = ethers.Wallet.createRandom()
     const brokerAccount = ethers.Wallet.createRandom()
@@ -50,6 +43,7 @@ describe('StorageConfig', () => {
     })
 
     beforeEach(async () => {
+        const engineAndEditorAccount = ethers.Wallet.createRandom()
         tracker = await startTracker({
             host: NODE_HOST,
             port: TRACKER_PORT,
@@ -62,6 +56,7 @@ describe('StorageConfig', () => {
             trackerPort: TRACKER_PORT,
             httpPort: HTTP_PORT,
             streamrUrl: STREAMR_URL,
+            streamrAddress: engineAndEditorAccount.address,
             enableCassandra: true
         })
         broker = await startBroker({
@@ -76,21 +71,22 @@ describe('StorageConfig', () => {
         client = createClient(WS_PORT, {
             auth: {
                 privateKey: publisherAccount.privateKey
-            },
-            restUrl: API_URL
+            }
         })
+        assignmentEventManager = new StorageAssignmentEventManager(WS_PORT, engineAndEditorAccount)
+        await assignmentEventManager.createStream()
     })
 
     afterEach(async () => {
-        await Promise.allSettled([storageNode.close(), broker.close(), tracker.stop()])
+        await Promise.allSettled([storageNode.close(), broker.close(), tracker.stop(), assignmentEventManager.close()])
     })
 
     it('when client publishes a message, it is written to the store', async () => {
         stream = await client.createStream({
             id: publisherAccount.address + '/StorageConfigTest/' + Date.now()
         })
-        await addStreamToStorageNode(stream.id, storageNodeAccount.address, client)
-        await waitForCondition(() => isPersistent(stream.id))
+        await assignmentEventManager.addStreamToStorageNode(stream.id, storageNodeAccount.address, client)
+        await waitForStreamPersistedInStorageNode(stream.id, 0, NODE_HOST, HTTP_PORT)
         const publishMessage = await client.publish(stream.id, {
             foo: 'bar'
         })
